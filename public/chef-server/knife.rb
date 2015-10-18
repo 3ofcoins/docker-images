818de08d3b06f52b84f1e2fbe3bd2dbebc9e388b
Maciej Pasternacki <maciej@3ofcoins.net>
2015-10-18 20:06:20 +0200

    Moved the image to separate repo.

--- public/chef-server/knife.rb
+++
@@ -1,3 +0,0 @@
-instance_eval(File.read("/etc/opscode/pivotal.rb"))
-chef_server_url "#{chef_server_url}/organizations/#{ENV['CHEF_ORGANIZATION']}" if ENV['CHEF_ORGANIZATION']
-versioned_cookbooks true
